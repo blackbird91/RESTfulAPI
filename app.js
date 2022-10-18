99239fffef604dfeb1742c9605528dfbda7612f6
Decryptr <decryptr@Decryptrs-MacBook-Pro.local>
2022-10-18 11:24:18 -0400

    Added Validation

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// ToDo:
+// - Once form is submitted, make sure that there is not an already existing proposal 
+//   created by that user. If it is, we add to the proposal, if not, create the proposal
+// - Make "Image" proposal field so that it stores the correct image URL
+// - Compare number of votes in user file to number of 'members' from proposal
+// - Validate all input using Joi or look into validationErrors used for validation in NodeJS course
+
 const path = require('path');
 
 const express = require('express');
--- controllers/proposals.js
+++ controllers/proposals.js
@@ -1,4 +1,5 @@
 const Proposal = require('../models/proposals');
+const Joi = require('joi')
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-proposal', {
@@ -10,6 +11,22 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
+  const schema = {
+    title: Joi.string().min(5).required(),
+    image: Joi.string().required(),
+    description: Joi.string().min(50).max(500).required(),
+    date: Joi.date().required(),
+    tags: Joi.string().required(),
+    votes: Joi.array().required(),
+    members: Joi.number().required()
+  }
+
+  const { error } = Joi.validate(req.body, schema)
+  if (error){
+    res.status(400).send(error.details[0].message)
+    return
+  }
+
   const proposal = new Proposal(req.body);
   proposal.save();
   res.redirect('/');
